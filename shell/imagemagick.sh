1234467b9279558457aa7754ec372818a87467a2
tokzk <t@okzk.org>
2017-04-12 19:17:57 +0900

    Add settings for imagemagick

---
+++ shell/imagemagick.sh
@@ -0,0 +1,4 @@
+export PATH="/usr/local/opt/imagemagick@6/bin:$PATH"
+export LDFLAGS=-L/usr/local/opt/imagemagick@6/lib
+export CPPFLAGS=-I/usr/local/opt/imagemagick@6/include
+export PKG_CONFIG_PATH=/usr/local/opt/imagemagick@6/lib/pkgconfig
