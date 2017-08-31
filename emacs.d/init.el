2b54918a55c54fce4ceb11bab3e67d0159a88a32
okzk <tex314@gmail.com>
2017-08-31 12:48:14 +0900

    Fix emacs settings

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -16,6 +16,12 @@
     (goto-char (point-max))
     (eval-print-last-sexp)))
 
+
+(add-to-list 'el-get-recipe-path (locate-user-emacs-file "el-get-user/recipes"))
+;(el-get 'sync)
+
+;; M-x el-get-list-packsges
+
 (el-get-bundle auto-complete)
 (el-get-bundle bind-key)
 (el-get-bundle browse-kill-ring)
@@ -46,13 +52,16 @@
 (el-get-bundle yasnippet)
 
 ;; prog modes
+(el-get-bundle alchemist)
 (el-get-bundle coffee-mode)
+(el-get-bundle elixir)
 (el-get-bundle go-mode)
 (el-get-bundle js2-mode)
 (el-get-bundle json-mode)
-;(el-get-bundle jsx/jsx-mode.el)
+(el-get-bundle jsx/jsx-mode.el)
 (el-get-bundle less-css-mode)
 (el-get-bundle markdown-mode)
+(el-get-bundle fish-mode)
 ;(el-get-bundle markdown-mode+)
 ;(el-get-bundle elpa:slim-mode)
 (el-get-bundle php-mode)
@@ -84,3 +93,15 @@
 ;(require 'init-loader)
 (setq init-loader-show-log-after-init nil)
 (init-loader-load "~/.emacs.d/inits") 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (slim-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- emacs.d/inits/20_web-mode.el
+++ emacs.d/inits/20_web-mode.el
@@ -3,64 +3,75 @@
 (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.js[x]\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.vue\\'" . web-mode))
+
+
+(setq web-mode-enable-current-column-highlight t)
 
 (defun my-web-mode-hook ()
   "Hooks for Web mode."
-  (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2)
-  (setq web-mode-style-padding 1)
-  (setq web-mode-script-padding 1)
-  (setq web-mode-block-padding 0)
-  (setq web-mode-comment-style 2)
-  )
+  (setq web-mode-markup-indent-offset 2))
 (add-hook 'web-mode-hook  'my-web-mode-hook)
 
-;; web-mode
-(defun my-web-mode-hook ()
-  "Hooks for Web mode."
-  (setq web-mode-attr-indent-offset nil)
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-css-indent-offset 2)
-  (setq web-mode-code-indent-offset 2)
-  (setq web-mode-sql-indent-offset 2)
-  (setq indent-tabs-mode nil)
-  (setq tab-width 2))
-(add-hook 'web-mode-hook 'my-web-mode-hook)
+;; (defun my-web-mode-hook ()
+;;   "Hooks for Web mode."
+;;   (setq web-mode-markup-indent-offset 2)
+;;   (setq web-mode-css-indent-offset 2)
+;;   (setq web-mode-code-indent-offset 2)
+;;   (setq web-mode-style-padding 1)
+;;   (setq web-mode-script-padding 1)
+;;   (setq web-mode-block-padding 0)
+;;   (setq web-mode-comment-style 2)
+;;   )
+;; (add-hook 'web-mode-hook  'my-web-mode-hook)
 
-;; web-mode(php
-(defun my-web-mode-hook ()
-  "Hooks for Web mode."
-  (setq web-mode-attr-indent-offset nil)
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-css-indent-offset 2)
-  (setq web-mode-code-indent-offset 2)
-  (setq web-mode-sql-indent-offset 2)
-  (setq indent-tabs-mode nil)
-  (setq tab-width 2))
-(add-hook 'web-mode-hook 'my-web-mode-hook)
+;; ;; web-mode
+;; (defun my-web-mode-hook ()
+;;   "Hooks for Web mode."
+;;   (setq web-mode-attr-indent-offset nil)
+;;   (setq web-mode-markup-indent-offset 2)
+;;   (setq web-mode-css-indent-offset 2)
+;;   (setq web-mode-code-indent-offset 2)
+;;   (setq web-mode-sql-indent-offset 2)
+;;   (setq indent-tabs-mode nil)
+;;   (setq tab-width 2))
+;; (add-hook 'web-mode-hook 'my-web-mode-hook)
+
+;; ;; web-mode(php
+;; (defun my-web-mode-hook ()
+;;   "Hooks for Web mode."
+;;   (setq web-mode-attr-indent-offset nil)
+;;   (setq web-mode-markup-indent-offset 2)
+;;   (setq web-mode-css-indent-offset 2)
+;;   (setq web-mode-code-indent-offset 2)
+;;   (setq web-mode-sql-indent-offset 2)
+;;   (setq indent-tabs-mode nil)
+;;   (setq tab-width 2))
+;; (add-hook 'web-mode-hook 'my-web-mode-hook)
 
 
-;; M-x web-mode-indent で 2とか4を入力
-(defun web-mode-indent (num)
-  (interactive "nIndent: ")
-  (setq web-mode-markup-indent-offset num)
-  (setq web-mode-css-indent-offset num)
-  (setq web-mode-style-padding num)
-  (setq web-mode-code-indent-offset num)
-  (setq web-mode-script-padding num)
-  (setq web-mode-block-padding num)
-  )
-(web-mode-indent 4)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (slim-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; ;; M-x web-mode-indent で 2とか4を入力
+;; (defun web-mode-indent (num)
+;;   (interactive "nIndent: ")
+;;   (setq web-mode-markup-indent-offset num)
+;;   (setq web-mode-css-indent-offset num)
+;;   (setq web-mode-style-padding num)
+;;   (setq web-mode-code-indent-offset num)
+;;   (setq web-mode-script-padding num)
+;;   (setq web-mode-block-padding num)
+;;   )
+;; (web-mode-indent 4)
+;; (custom-set-variables
+;;  ;; custom-set-variables was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(package-selected-packages (quote (slim-mode))))
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  )
